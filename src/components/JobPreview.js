78d0d6af59366504e02c8b570f66ff2a9055e2fb
Zelimhan Ismailov <zelim-han@hotmail.com>
2021-03-05 15:17:54 +0100

    added second component the dashboard

---
+++ src/components/JobPreview.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class JobPreview extends Component {
+     
+    render() {
+        const { jobDetails } = this.props;
+        return (
+            <div>
+                <h1>works!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default JobPreview;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,52 +1,111 @@
 import React, { Component } from "react";
-import {Row, Container, Col} from 'react-bootstrap'
+import { Row, Container, Col } from 'react-bootstrap'
+import { Route, Link } from "react-router-dom";
+import '../styles/Landing.css'
+import JobPreview from './JobPreview'
 
 class Landing extends Component {
+
+  state = {
+    showPrev: false
+  }
+
+  showPreview = () => {
+    this.setState({
+      showPrev: true
+    })
+  }
+ 
+
   render() {
     const { jobDetails } = this.props;
+    console.log(jobDetails)
     return (
       <div>
         <Container>
           <Row>
             <Col>
               Job Offers
-                <div>
-                  {/* {
-                    jobDetails.map((detail) => {
-                      return (
-                        <div key={detail.}>
-                          {detail.jobTitle}
-                        </div>
-                      )
-                    })
-                  } */}
-                </div>
+              <div>
+                {jobDetails.map((detail) => {
+                  if (detail.userId == this.props.loggedInUser?._id) {
+                    return (
+                      <div key={detail._id}>
+                        <Link to={`/dashboard/${detail._id}`}>
+                          <div className="job-details">
+                            {detail.jobTitle}
+                            <br></br>
+                            {detail.companyName}
+                          </div>
+                        </Link>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
             </Col>
             <Col>
               <form onSubmit={this.props.onAdd}>
-              <input name="jobTitle" type="text" placeholder="Job Title"/>
-              <input name="companyName" type="text" placeholder="Company"/>
-              <br></br>
-              <label><strong>Application Date</strong></label>
-              <input type="date" id="start" name="applicationDate"/>
-              <br></br>
-              <input name="contactPerson" type="text" placeholder="Contact Person"/>
-              <input name="contactDetail" type="text" placeholder="Contact detail"/>
-              <input name="jobDescription" type="text" placeholder="Job Description"/>
-              {/* <input name="companyRating" type="text" placeholder="Company Rating"/> */}
-              <input name="applicationLink" type="text" placeholder="Application Link" />
-              <input name="sourceOfApplication" type="text" placeholder="Source of Application"/>
-              <input name="salary" type="number" placeholder="Salary"/>
-              <br></br>
-              <label><strong>Interview Date</strong></label>
-              {/* <input type="date" id="start" name="interviewDate"/> */}
-              <br></br>
-              <input name="jobLocation" type="text" placeholder="Job Location"/>
-              <br></br>
-              <button type="submit">Submit</button>
+                <input name="jobTitle" type="text" placeholder="Job Title" />
+                <input name="companyName" type="text" placeholder="Company" />
+                <br></br>
+                <label>
+                  <strong>Application Date</strong>
+                </label>
+                <input type="date" id="start" name="applicationDate" />
+                <br></br>
+                <input
+                  name="contactPerson"
+                  type="text"
+                  placeholder="Contact Person"
+                />
+                <input
+                  name="contactDetail"
+                  type="text"
+                  placeholder="Contact detail"
+                />
+                <input
+                  name="jobDescription"
+                  type="text"
+                  placeholder="Job Description"
+                />
+                {/* <input name="companyRating" type="text" placeholder="Company Rating"/> */}
+                <input
+                  name="applicationLink"
+                  type="text"
+                  placeholder="Application Link"
+                />
+                <input
+                  name="sourceOfApplication"
+                  type="text"
+                  placeholder="Source of Application"
+                />
+                <input name="salary" type="number" placeholder="Salary" />
+                <br></br>
+                <label>
+                  <strong>Interview Date</strong>
+                </label>
+                {/* <input type="date" id="start" name="interviewDate"/> */}
+                <br></br>
+                <input
+                  name="jobLocation"
+                  type="text"
+                  placeholder="Job Location"
+                />
+                <br></br>
+                <button type="submit">Submit</button>
               </form>
             </Col>
+            <br></br>
           </Row>
+          <div>
+            <Route
+              exact
+              path={"/dashboard/:jobId"}
+              jobDetails={jobDetails}
+              component={JobPreview}
+            />
+          </div>
         </Container>
       </div>
     );
