346e1b29ffc09d6406a6ea901135ff589101f4dc
Benjamin megret <benji@MacBook-Pro-de-Benjamin.local>
2021-03-07 11:05:55 +0100

    update job decription

--- src/components/ShowSteps.js
+++ src/components/ShowSteps.js
@@ -50,13 +50,24 @@ class ShowSteps extends Component {
   
     render() {
         console.log(this.state.stepsArray)
+        const {stepsArray} = this.props
         return (
             <div>
                 <div className="card1">
                     <div>
                       <h4>Your steps</h4>
                         <div className="scrollDown">
-                          <p className="un">I called the HR to set-up an interview  03/03/2021 <button className="deleteStep">Delete</button></p>
+                        {
+                            stepsArray.map((oneStep) => {
+                                if(oneStep.stepsId == this.props.loggedInUser?._id) {
+                                    return (
+                                        <p className="un"> {oneStep.description} {oneStep.date} <button className="deleteStep">Delete</button></p>
+                                    )
+                                }
+                            })
+                        }
+                        
+                          
                         </div>
                     </div>
                 </div>
