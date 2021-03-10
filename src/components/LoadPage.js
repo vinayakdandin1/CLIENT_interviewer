7873f3bb87d91f59af574509a7256f7f875e8634
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-03-10 22:19:16 +0100

    Sign in responsive completed

--- src/components/LoadPage.js
+++ src/components/LoadPage.js
@@ -9,7 +9,20 @@ class LoadPage extends Component {
     
     state = {
         hasClass: true,
-        classes: "cont"
+        classes: "cont",
+        showSignUp: false
+    }
+
+    showSignUp = () => {
+      this.setState({
+        showSignUp: true
+      })
+    }
+
+    showSignIn = () => {
+      this.setState({
+        showSignUp: false
+      })
     }
 
     classToggle = () => {    
@@ -19,58 +32,11 @@ class LoadPage extends Component {
     render() {
         return (
           <div className="landing-main">
-            <div className={this.state.classes}>
-              <div className="form sign-in">
-                <h2>Welcome back,</h2>
-                <form onSubmit={this.props.onSignIn}>
-                  <label className="inLabels">
-                    <span>Email</span>
-                    <input name="emailId" type="email" />
-                  </label>
-                  <label className="inLabels">
-                    <span>Password</span>
-                    <input name="password" type="password" />
-                  </label>
-                  <button type="submit" className="submit">
-                    Sign In
-                  </button>
-                </form>
-                {/* <button type="button" className="fb-btn">Connect with <span>Google</span></button> */}
-                <GoogleLogin 
-                      clientId={`${process.env.REACT_APP_CLIENT_ID}`}
-                      buttonText="Login"
-                      onSuccess={this.props.googleSignIn}
-                      onFailure={this.props.errorGoogleSignIn}
-                      cookiePolicy={'single_host_origin'}
-                  />
-              </div>
-
-              <div className="sub-cont">
-                <div className="img">
-                  <div className="img__text m--up">
-                    <h2>New here?</h2>
-                    <p>Sign up and manage your interviews!</p>
-                  </div>
-                  <div className="img__text m--in">
-                    <h2>One of us?</h2>
-                    <p>If you already have an account, just sign in!</p>
-                  </div>
-                  <div onClick={this.classToggle} className="img__btn">
-                    <span className="m--up">Sign Up</span>
-                    <span className="m--in">Sign In</span>
-                  </div>
-                </div>
-                <div className="form sign-up">
-                  <h2>Time to manage your interview Schedules,</h2>
-                  <form onSubmit={this.props.onSignUp}>
-                    <label className="inLabels">
-                      <span>First Name</span>
-                      <input name="firstName" type="text" />
-                    </label>
-                    <label className="inLabels">
-                      <span>Last Name</span>
-                      <input name="lastName" type="text" />
-                    </label>
+            <div className="landing-responsive">
+              <div className={this.state.classes}>
+                <div className="form sign-in">
+                  <h2>Welcome back,</h2>
+                  <form onSubmit={this.props.onSignIn}>
                     <label className="inLabels">
                       <span>Email</span>
                       <input name="emailId" type="email" />
@@ -80,29 +46,127 @@ class LoadPage extends Component {
                       <input name="password" type="password" />
                     </label>
                     <button type="submit" className="submit">
-                      Sign Up
+                      Sign In
                     </button>
                   </form>
-                  {/* <button type="button" className="fb-btn">Join with <span>Google</span></button> */}
+                  {/* <button type="button" className="fb-btn">Connect with <span>Google</span></button> */}
+                  {/* <GoogleLogin 
+                        clientId={`${process.env.REACT_APP_CLIENT_ID}`}
+                        buttonText="Login with Google"
+                        onSuccess={this.props.googleSignIn}
+                        onFailure={this.props.errorGoogleSignIn}
+                        cookiePolicy={'single_host_origin'}
+                    /> */}
+                </div>
+
+                <div className="sub-cont">
+                  <div className="img">
+                    <div className="img__text m--up">
+                      <h2>New here?</h2>
+                      <p>Sign up and manage your interviews!</p>
+                    </div>
+                    <div className="img__text m--in">
+                      <h2>One of us?</h2>
+                      <p>If you already have an account, just sign in!</p>
+                    </div>
+                    <div onClick={this.classToggle} className="img__btn">
+                      <span className="m--up">Sign Up</span>
+                      <span className="m--in">Sign In</span>
+                    </div>
+                  </div>
+                  <div className="form sign-up">
+                    <h2>Time to manage your interview Schedules,</h2>
+                    <form onSubmit={this.props.onSignUp}>
+                      <label className="inLabels">
+                        <span>First Name</span>
+                        <input name="firstName" type="text" />
+                      </label>
+                      <label className="inLabels">
+                        <span>Last Name</span>
+                        <input name="lastName" type="text" />
+                      </label>
+                      <label className="inLabels">
+                        <span>Email</span>
+                        <input name="emailId" type="email" />
+                      </label>
+                      <label className="inLabels">
+                        <span>Password</span>
+                        <input name="password" type="password" />
+                      </label>
+                      <button type="submit" className="submit">
+                        Sign Up
+                      </button>
+                    </form>
+                    {/* <button type="button" className="fb-btn">Join with <span>Google</span></button> */}
+                    
+                    <GoogleLogin 
+                        clientId={`${process.env.REACT_APP_CLIENT_ID}`}
+                        buttonText="SignUp with Google"
+                        onSuccess={this.props.googleSignUp}
+                        onFailure={this.props.googleSignUp}
+                        cookiePolicy={'single_host_origin'}
+                    />
                   
-                  <GoogleLogin 
-                      clientId={`${process.env.REACT_APP_CLIENT_ID}`}
-                      buttonText="SignUp"
-                      onSuccess={this.props.googleSignUp}
-                      onFailure={this.props.googleSignUp}
-                      cookiePolicy={'single_host_origin'}
-                  />
-                 
+                  </div>
                 </div>
               </div>
+              
             </div>
 
-            {/* <a rel="noreferrer" href="https://dribbble.com/shots/3306190-Login-Registration-form" target="_blank" className="icon-link">
-                <img src="http://icons.iconarchive.com/icons/uiconstock/socialmedia/256/Dribbble-icon.png" alt="" />
-                </a>
-                <a rel="noreferrer" href="https://twitter.com/NikolayTalanov" target="_blank" className="icon-link icon-link--twitter">
-                <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/twitter-128.png" alt="" />
-                </a> */}
+            <div className="landing-mobile">
+                  
+
+                  {(this.state.showSignUp) ? 
+                  
+                    (<div className="landing">
+                    <form onSubmit={this.props.onSignUp}>
+                        <label className="inLabels">
+                          <span>First Name</span>
+                          <input name="firstName" type="text" />
+                        </label>
+                        <label className="inLabels">
+                          <span>Last Name</span>
+                          <input name="lastName" type="text" />
+                        </label>
+                        <label className="inLabels">
+                          <span>Email</span>
+                          <input name="emailId" type="email" />
+                        </label>
+                        <label className="inLabels">
+                          <span>Password</span>
+                          <input name="password" type="password" />
+                        </label>
+                        <button type="submit" className="submit">
+                          Sign Up
+                        </button>
+                      </form>
+                      <div>
+                        <h2>One of us?</h2>
+                        <button onClick={this.showSignIn} className="submit" >Sign in</button> 
+                      </div> 
+                  </div>
+                  ) : (
+                    <div className="landing-mobile-signin">
+                      <form onSubmit={this.props.onSignIn}>
+                        <label className="inLabels">
+                          <span>Email</span>
+                          <input name="emailId" type="email" />
+                        </label>
+                        <label className="inLabels">
+                          <span>Password</span>
+                          <input name="password" type="password" />
+                        </label>
+                        <button type="submit" className="submit">
+                          Sign In
+                        </button>
+                      </form> 
+                      <div>
+                        <h2>New here?</h2>
+                        <button onClick={this.showSignUp} className="submit" >Sign up</button> 
+                      </div>              
+                    </div>
+                  )}
+              </div>
             <div>
               {this.props.unloggedUser ? <h1>Please Log in First</h1> : null}
             </div>
